d4f3409fc2a30a5d6a7126295429d6b5db65f609
Jahid Hassan Munna <mdjahidhassan.41296@gmail.comgit config --global user.name Your>
2020-10-27 12:25:20 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+/*let pakhi;
+console.log(pakhi)*/
+
+
+/*function add(num1,num2){
+    console.log(num1+num2)
+    return ;
+}
+const result= add(13,12)
+console.log(result)*/
+
+const premik= {name:"smart guy", phone:01703116479}
+console.log(premik.gf)
+
+let ages=[4,5,8]
+console.log(ages[23])
