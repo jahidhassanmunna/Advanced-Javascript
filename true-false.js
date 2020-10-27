c5d3ccb9935a891e8a3273ce943df2a5100cd1f1
Jahid Hassan Munna <mdjahidhassan.41296@gmail.comgit config --global user.name Your>
2020-10-27 11:45:24 +0600

    truthy & falsy

---
+++ true-false.js
@@ -0,0 +1,15 @@
+/*const name=" "
+if(name){
+    console.log("this is true")
+}
+else{
+    console.log("this is false")
+}
+*/
+let num=12;
+if( num==0){
+    console.log("this is true")
+}
+else{
+    console.log("this is false")
+}
